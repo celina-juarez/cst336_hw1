6cd3a44267609f8da02d25daaa2b99d021cba1a9
Celina Juarez <cejuarez@csumb.edu>
2018-02-08 04:19:04 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+        <title id = "title"> CS Topic: Programming Languages </title>
+        
+        <style>
+            body{
+                background-color: #a5acaf !important;
+            }
+        </style>
+        
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+    </head>
+    
+    <body id = "first">
+        
+        <header>
+            <h1> Programming Languages </h1>
+        </header>
+        
+        <nav>
+            <hr width = "50%" />
+            <a href = "index.html">Home</a>
+            <a href = "about-cplusplus.html">C++</a>
+            <a href = "about-python.html">Python</a>
+            <a href = "about-ruby.html">Ruby</a>
+        </nav>
+        
+        <br /><br />
+        
+        <main>
+            
+            <figure id="languages">
+                <img src = "img/languages.png" alt = "Collage of Programming Languages" />
+                <img src = "img/learn.png" alt = "Learn to Code"/>
+            </figure>
+            
+            <div id="welcomeText">
+                Hello! <br/>
+                <p> Thank you for visiting this CS Topics website.</p>
+                <p> Want to learn more about different programming languages?</p>
+                <p> Then your'e at the right place.</p>
+                <p> Programming languages allow us to communicate instructions to a computer in a way the computer can understand.</p>
+                <p> These instructions allow a computer to compute several actions quickly and efficiently.</p>
+                <p> There are a variety of programming languages, however we will only focus on three</p>
+                <p> <b>C++, Python, and Ruby</b></p>
+                Click <a href="https://www.computerscience.org/resources/computer-programming-languages/">here</a> to explore the sources used for this website.
+            </div>
+        </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            
+            <hr>
+            CST 336. 2018&copy; Juarez <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitious.<br />
+            It is used for academic purposes only;
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
